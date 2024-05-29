77cd9c9b70e04943fc6bfa138ab1bf681f2c2892
Lukz770 <72799605+lucasbr237@users.noreply.github.com>
2024-05-29 06:56:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import telebot
 from gradio_client import Client
-from flask import Flask, request, abort
+
 
 # Substitua pelo token do seu bot
 API_TOKEN = '6911510369:AAHzz8jiLZIPElv_cA241cE7CbGj1pUsuUc'
+
+# app.py
+
+import telebot
+from gradio_client import Client
+
+# Substitua pelo token do seu bot
+API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
 
 # Inicializa o bot do Telegram
 bot = telebot.TeleBot(API_TOKEN)
@@ -11,22 +19,7 @@ bot = telebot.TeleBot(API_TOKEN)
 # Inicializa o cliente do Gradio
 client = Client("https://lukz770-chat-luna.hf.space")
 
-# Inicializa o aplicativo Flask
-app = Flask(__name__)
-
-# Rota para receber as mensagens do bot do Telegram
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        abort(403)
-
 # Função para tratar mensagens de texto
-@bot.message_handler(func=lambda message: True)
 def handle_message(message):
     user_input = message.text
 
@@ -36,7 +29,7 @@ def handle_message(message):
     # Envia a mensagem para o chatbot e recebe a resposta
     result = client.predict(
         message=user_input,
-        request="You are Ayla a  friendly Chatbot, and you speak Portuguese.",
+        request="You are Ayla a friendly Chatbot, and you speak Portuguese.",
         param_3=512,
         param_4=0.2,
         param_5=0.95,
@@ -46,6 +39,5 @@ def handle_message(message):
     # Envia a resposta do chatbot de volta ao usuário no Telegram
     bot.reply_to(message, result)
 
-# Inicia o bot
-if __name__ == '__main__':
-    app.run(debug=True)
+# Exponha a função para o Gunicorn
+app = handle_message
